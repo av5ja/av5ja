90389b53e6c763259bd0cc4bcb0562b497ade4d0
tkgstrator <nasawake.am@gmail.com>
2023-08-28 13:39:06 +0900

    BulletTokenの追加

---
+++ src/requests/bullet_token.ts
@@ -0,0 +1,34 @@
+import { Transform, plainToInstance } from 'class-transformer';
+
+import { JWT, Token } from '../dto/jwt.dto';
+import { ResponseType, RequestType, Method, Headers, Parameters } from '../utils/request';
+
+export namespace BulletToken {
+  export class Request implements RequestType {
+    readonly baseURL: string = 'https://api.lp1.av5ja.srv.nintendo.net/';
+    readonly headers: Headers;
+    readonly method: Method = Method.POST;
+    readonly parameters: Parameters;
+    readonly path: string = 'api/bullet_tokens';
+
+    constructor(token: JWT<Token.GameWebToken>, version: string) {
+      this.headers = {
+        'Content-Type': 'application/json',
+        'X-GameWebToken': token.rawValue,
+        'X-NaCountry': 'US',
+        'X-Web-View-Ver': version,
+      };
+    }
+
+    request(response: any): ResponseType {
+      return plainToInstance(Response, response, { excludeExtraneousValues: false });
+    }
+  }
+
+  export class Response implements ResponseType {
+    readonly bullet_token: string;
+    readonly lang: string;
+    @Transform(({ value }) => value === 'true')
+    readonly is_noe_country: boolean;
+  }
+}
--- tests/authorize.spec.ts
+++ tests/authorize.spec.ts
@@ -1,5 +1,6 @@
 import { JWT, Token } from '../src/dto/jwt.dto';
 import { AccessToken } from '../src/requests/access_token';
+import { BulletToken } from '../src/requests/bullet_token';
 import { CoralToken } from '../src/requests/coral_token';
 import { GameServiceToken } from '../src/requests/game_service_token';
 import { GameWebToken } from '../src/requests/game_web_token';
@@ -29,6 +30,7 @@ describe('Authorize', () => {
 
   it('Game Web Token', async () => {
     const version: string = '2.7.0';
+    const web_version: string = '4.0.0-b8c1e0fc';
     const access_token = (await request(new AccessToken.Request(session_token))) as AccessToken.Response;
     const coral_token_nso: CoralToken.Response = await request(
       new CoralToken.Request(access_token.access_token.rawValue, 1, access_token.na_id, undefined, '2.7.0')
@@ -37,9 +39,8 @@ describe('Authorize', () => {
     const coral_token_app: CoralToken.Response = await request(
       new CoralToken.Request(game_service_token.access_token.rawValue, 2, access_token.na_id, game_service_token.access_token.payload.sub, '2.7.0')
     );
-    const game_web_token = (
-      (await request(new GameWebToken.Request(game_service_token.access_token, coral_token_app, version))) as GameWebToken.Response
-    )
+    const game_web_token = (await request(new GameWebToken.Request(game_service_token.access_token, coral_token_app, version))) as GameWebToken.Response;
+    const bullet_token = (await request(new BulletToken.Request(game_web_token.access_token, web_version))) as BulletToken.Response;
 
     // NA_ID
     expect(access_token.na_id).toBe('5ae8f7a78b0cca4d');
@@ -49,5 +50,7 @@ describe('Authorize', () => {
     expect(game_service_token.nsa_id).toBe(game_web_token.nsa_id);
     expect(game_web_token.access_token.payload.aud).toBe('6633677291552768');
     expect(game_web_token.access_token.payload.typ).toBe('id_token');
+    expect(bullet_token.lang).toBe('ja-JP');
+    expect(bullet_token.is_noe_country).toBe(false);
   }, 10000);
 });
