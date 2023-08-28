61129af6bf36b259ee547443895ede953ee7de54
tkgstrator <nasawake.am@gmail.com>
2023-08-28 11:31:36 +0900

    リクエストのテストの追加

--- src/dto/jwt.dto.ts
+++ src/dto/jwt.dto.ts
@@ -1,87 +1,90 @@
-import { Transform } from 'class-transformer'
 
 class Header {
-    readonly alg: AlgorithmType
+  readonly alg: AlgorithmType;
 }
 
 interface PayloadType {
-    readonly aud: string
-    readonly exp: number
-    readonly typ: TokenType
-    readonly iat: number
+  readonly aud: string;
+  readonly exp: number;
+  readonly iat: number;
+  get isValid(): boolean;
 
-    get isValid(): boolean
+  readonly typ: TokenType;
 }
 
-export class SessionToken implements PayloadType {
-    readonly aud: string
-    readonly exp: number
-    readonly typ: TokenType
-    readonly iat: number
+export namespace Token {
+  export class SessionToken implements PayloadType {
+    readonly aud: string;
+    readonly exp: number;
+    readonly typ: TokenType;
+    readonly iat: number;
 
     get isValid(): boolean {
-        return true
+      return true;
     }
+  }
+
+  export class Token implements PayloadType {
+    readonly aud: string;
+    readonly exp: number;
+    readonly typ: TokenType;
+    readonly iat: number;
+    readonly sub: string;
+
+    get isValid(): boolean {
+      return true;
+    }
+  }
+
+  export class GameServiceToken implements PayloadType {
+    readonly aud: string;
+    readonly exp: number;
+    readonly typ: TokenType;
+    readonly iat: number;
+    readonly membership: Membership;
+    readonly isChildRestricted: boolean;
+
+    get isValid(): boolean {
+      return true;
+    }
+  }
+
+  export class GameWebToken implements PayloadType {
+    readonly aud: string;
+    readonly exp: number;
+    readonly typ: TokenType;
+    readonly iat: number;
+    readonly links: Links;
+
+    get isValid(): boolean {
+      return true;
+    }
+  }
+
 }
 
 interface NetworkServiceAccount {
-    readonly id: string
+  readonly id: string;
 }
 
 interface Links {
-    readonly networkServiceAccount: NetworkServiceAccount
+  readonly networkServiceAccount: NetworkServiceAccount;
 }
 
 interface Membership {
-    readonly active: boolean
+  readonly active: boolean;
 }
 
-export class Token implements PayloadType {
-    readonly aud: string
-    readonly exp: number
-    readonly typ: TokenType
-    readonly iat: number
-    readonly sub: string
-
-    get isValid(): boolean {
-        return true
-    }
-}
-
-export class GameServiceToken implements PayloadType {
-    readonly aud: string
-    readonly exp: number
-    readonly typ: TokenType
-    readonly iat: number
-    readonly membership: Membership
-    readonly isChildRestricted: boolean
-
-    get isValid(): boolean {
-        return true
-    }
-}
-
-export class GameWebToken implements PayloadType {
-    readonly aud: string
-    readonly exp: number
-    readonly typ: TokenType
-    readonly iat: number
-    readonly links: Links
-
-    get isValid(): boolean {
-        return true
-    }
-}
 
 export class JWT<T extends PayloadType> {
-    header: Header
-    payload: T
-    rawValue: string
+  header: Header;
+  payload: T;
+  rawValue: string;
 
-    constructor(rawValue: string) {
-        this.rawValue = rawValue
-        const [header, payload, signature] = rawValue.split('.')
-        this.header = JSON.parse(atob(header))
-        this.payload = JSON.parse(atob(payload)) as T
-    }
+  constructor(rawValue: string) {
+    this.rawValue = rawValue;
+    const [header, payload, signature] = rawValue.split('.');
+    this.header = JSON.parse(atob(header));
+    this.payload = JSON.parse(atob(payload)) as T;
+  }
 }
---
+++ src/requests/access_token.ts
@@ -0,0 +1,37 @@
+import { plainToInstance } from "class-transformer";
+
+import { JWT, Token } from "../dto/jwt.dto";
+import { ResponseType, RequestType, Method, Headers, Parameters } from "../utils/request";
+
+export namespace AccessToken {
+    export class Request implements RequestType {
+        readonly baseURL: string = 'https://accounts.nintendo.com/';
+        readonly headers: Headers = {
+            'Content-Type': 'application/json',
+        }
+        readonly method: Method = Method.POST;
+        readonly parameters: Parameters
+        readonly path: string = 'connect/1.0.0/api/token'
+
+        constructor(session_token: string) {
+            this.parameters = {
+                client_id: "71b963c1b7b6d119",
+                grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer-session-token',
+                session_token: session_token
+            }
+        }
+
+        request(response: any): ResponseType {
+            console.log(response)
+            return plainToInstance(Response, response, { excludeExtraneousValues: false })
+        }
+    }
+
+    export class Response implements ResponseType {
+        access_token: JWT<Token.Token>
+        expires_in: number
+        id_token: JWT<Token.Token>
+        scope: string[]
+        token_type: string
+    }
+}
