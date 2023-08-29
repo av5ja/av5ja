7e5762b9769939c224877a7581c1e767ec005c30
tkgstrator <nasawake.am@gmail.com>
2023-08-30 02:06:58 +0900

    CoopHistoryDetailQueryに対応

--- tests/graphql.spec.ts
+++ tests/graphql.spec.ts
@@ -1,5 +1,6 @@
 import { CoopHistoryDetailQuery } from '../src/requests/av5ja/coop_history_detail_query';
 import { CoopHistoryQuery } from '../src/requests/av5ja/coop_history_query';
+import { StageScheduleQuery } from '../src/requests/av5ja/stage_schedule_query';
 import { Common } from '../src/utils/common';
 import { request } from '../src/utils/graph_ql';
 
@@ -8,24 +9,24 @@ import token from './token.json';
 describe('GraphQL', () => {
     const bullet_token = token.bullet_token;
 
-    // it('CoopHistoryQuery', async () => {
-    //     const coop_history_query = await request(new CoopHistoryQuery.Request(), bullet_token);
-    //     expect(coop_history_query.data.coop_result.history_groups.nodes.length).toBe(4);
-    //     expect(coop_history_query.results.length).toBe(50);
+    it('CoopHistoryQuery', async () => {
+        const coop_history_query = await request(new CoopHistoryQuery.Request(), bullet_token);
+        expect(coop_history_query.data.coop_result.history_groups.nodes.length).toBe(4);
+        expect(coop_history_query.results.length).toBe(50);
 
-    //     const result_id_list: Common.CoopHistoryDetailId[] = coop_history_query.result_id_list;
-    //     /**
-    //      * 変換後のリザルトIDが正しいことを確認する
-    //      */
-    //     result_id_list.forEach((result_id: Common.CoopHistoryDetailId) => {
-    //         expect(result_id.rawValue).toBe(result_id.desciption);
-    //     });
-    // }, 5000);
+        const result_id_list: Common.CoopHistoryDetailId[] = coop_history_query.result_id_list;
+        /**
+         * 変換後のリザルトIDが正しいことを確認する
+         */
+        result_id_list.forEach((result_id: Common.CoopHistoryDetailId) => {
+            expect(result_id.rawValue).toBe(result_id.rawValue);
+        });
+    }, 5000);
 
-    // it('StageScheduleQuery', async () => {
-    //     const stage_schedule_query = await request(new StageScheduleQuery.Request(), bullet_token);
-    //     expect(stage_schedule_query.schedules.length).toBe(4);
-    // }, 5000);
+    it('StageScheduleQuery', async () => {
+        const stage_schedule_query = await request(new StageScheduleQuery.Request(), bullet_token);
+        expect(stage_schedule_query.schedules.length).toBe(4);
+    }, 5000);
 
     it('CoopHistoryDetailQuery', async () => {
         const coop_history_query = await request(new CoopHistoryQuery.Request(), bullet_token);
@@ -33,7 +34,8 @@ describe('GraphQL', () => {
         expect(coop_history_query.results.length).toBe(50);
 
         const result_id_list: Common.CoopHistoryDetailId[] = coop_history_query.result_id_list;
-        const result = (await request(new CoopHistoryDetailQuery.Request(result_id_list[0].rawValue), bullet_token)) as CoopHistoryDetailQuery.Response;
-        console.log(JSON.stringify(result.data.coop_history_detail.member_results, null, 2));
-    }, 10000);
+        result_id_list.forEach(async (result_id: Common.CoopHistoryDetailId) => {
+            const result = (await request(new CoopHistoryDetailQuery.Request(result_id.rawValue), bullet_token)) as CoopHistoryDetailQuery.Response;
+        })
+    }, 50000);
 });
