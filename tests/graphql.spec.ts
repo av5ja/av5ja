b675e9bfd1b61621b5c6d350273ea2bccbe2e6f8
tkgstrator <nasawake.am@gmail.com>
2023-08-31 15:36:28 +0900

    Keychainから読みだした認証情報でアクセスする

--- tests/graphql.spec.ts
+++ tests/graphql.spec.ts
@@ -1,9 +1,47 @@
+import { CapacitorHttp, HttpOptions } from '@capacitor/core';
+import snakecaseKeys from 'snakecase-keys';
+
+import { Method } from '../src/enum/method';
 import { CoopHistoryDetailQuery } from '../src/requests/av5ja/coop_history_detail_query';
 import { CoopHistoryQuery } from '../src/requests/av5ja/coop_history_query';
 import { StageScheduleQuery } from '../src/requests/av5ja/stage_schedule_query';
-import { request } from '../src/utils/graph_ql';
+import { GraphQL } from '../src/utils/graph_ql';
 
 import token from './token.json';
+
+/**
+ * GraphQLリクエストを送信する
+ * @param request リクエスト
+ * @param bullet_token トークン
+ * @returns レスポンス
+ */
+async function request<T extends GraphQL, U extends ReturnType<T['request']>>(request: T, bullet_token: string): Promise<U> {
+    const url = new URL('https://api.lp1.av5ja.srv.nintendo.net/api/graphql');
+    const body = JSON.stringify({
+        extensions: {
+            persistedQuery: {
+                sha256Hash: request.hash,
+                version: 1,
+            },
+        },
+        variables: request.parameters,
+    });
+    const version = '4.0.0-b8c1e0fc';
+    const headers = {
+        Authorization: `Bearer ${bullet_token}`,
+        'Content-Type': 'application/json',
+        'X-Web-View-Ver': version,
+    };
+    const options: HttpOptions = {
+        data: body,
+        headers: headers,
+        method: Method.POST,
+        responseType: 'json',
+        url: url.href,
+    };
+    const response = await CapacitorHttp.request(options);
+    return request.request(snakecaseKeys(response.data)) as U;
+}
 
 describe('GraphQL', () => {
     const bullet_token = token.bullet_token;
@@ -15,7 +53,7 @@ describe('GraphQL', () => {
 
     it('StageScheduleQuery', async () => {
         const stage_schedule_query = await request(new StageScheduleQuery.Request(), bullet_token);
-        expect(stage_schedule_query.schedules.length).toBe(4);
+        expect(stage_schedule_query.schedules.length).toBe(3);
     }, 5000);
 
     it('CoopHistoryDetailQuery', async () => {
