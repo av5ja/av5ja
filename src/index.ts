bcf307840dddf2700164df4a4d7615430540ba67
tkgstrator <nasawake.am@gmail.com>
2023-08-31 18:01:16 +0900

    メソッドをクライアントから利用できるようにする

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export * from './utils/oauth';
+export { request, Headers, Parameters, GraphQL } from './utils/graph_ql';
+export { CoopHistoryDetailQuery } from './requests/av5ja/coop_history_detail_query';
+export { CoopHistoryQuery } from './requests/av5ja/coop_history_query';
+export { StageScheduleQuery } from './requests/av5ja/stage_schedule_query';
+export { SHA256Hash } from './enum/sha256hash';
