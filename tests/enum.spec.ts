07f8ffb821f6e4b193adce728d5696b93519ee7d
tkgstrator <nasawake.am@gmail.com>
2023-09-01 16:06:55 +0900

    Id, Hashの実装

---
+++ tests/enum.spec.ts
@@ -0,0 +1,20 @@
+import { WeaponInfoMain, hash, id } from "../src/utils/weapon_type";
+
+describe('Enum', () => {
+    const verifier: string = 'OwaTAOolhambwvY3RXSD-efxqdBEVNnQkc0bBJ7zaak';
+
+    describe('WeaponInfoMain', () => {
+        it('Id', () => {
+            const all_cases: WeaponInfoMain.Id[] = Object.values(WeaponInfoMain.Id).filter((id) => !isNaN(id as number)) as WeaponInfoMain.Id[];
+            console.log(all_cases.map((id) => hash(id)))
+            expect(all_cases.length).toBe(69);
+            
+        });
+
+        it('Hash', () => {
+            const all_cases = Object.values(WeaponInfoMain.Hash)
+            console.log(all_cases.map((hash) => id(hash)))
+            expect(all_cases.length).toBe(69);
+        });
+    })
+});
