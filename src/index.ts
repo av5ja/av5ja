2517b01ce3112b41915fb2ea3e78bb47209bda06
tkgstrator <nasawake.am@gmail.com>
2023-09-20 08:11:59 +0900

    Add export functions (#16)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { authorize, oauthURL } from './utils/oauth';
-export { get_coop_history_details } from './requests/requests';
+export { get_coop_history_details, get_coop_schedules } from './requests/requests';
 export { CoopResult } from './requests/stats/coop_result';
 export { CoopSchedule } from './requests/stats/coop_schedule';
